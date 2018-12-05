b6f4572fdcdcfca7baf61a2f41e29bcb8dea39d8
chihyu <chihyu@cht.com.tw>
2018-12-05 21:25:47 +0800

    修改訂單狀態修改

--- src/main/java/com/waterproof/bjb/shopping/repository/CustomerOrderRepository.java
+++ src/main/java/com/waterproof/bjb/shopping/repository/CustomerOrderRepository.java
@@ -7,19 +7,21 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.waterproof.bjb.shopping.entity.CustomerOrder;
 
 @Repository
 public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Integer> {
-	
+
 	public List<CustomerOrder> findByUsernameOrderByInsertedDesc(String username);
-	
+
 	public CustomerOrder findByOrderNoOrderByInsertedDesc(String orderNo);
-	
+
 	public CustomerOrder findByIdOrderByInsertedDesc(int id);
-	
+
 	@Modifying
+	@Transactional
 	@Query("update CustomerOrder co set co.statusId =:statusId  where co.orderNo = :orderNo")
 	public void setCustomerOrderByOrderNo(@Param("statusId") int statusId, @Param("orderNo") String orderNo);
 }
